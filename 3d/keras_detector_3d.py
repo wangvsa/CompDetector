ee0beb77e44aceb876358049f6baadfd326962fa
Chen Wang <wangvsa@163.com>
2019-03-13 10:02:27 -0500

    Refactoring arguments parsing function

--- 3d/keras_detector_3d.py
+++ 3d/keras_detector_3d.py
@@ -12,7 +12,8 @@ NX, NY, NZ = 16, 16, 16
 WINDOWS_PER_IMAGE = 64*64*64/NX/NY/NZ
 #variables = ["dens", "pres", "temp"]
 variables = ["dens"]
-EPOCHS = 2
+BATCH_SIZE = 64
+
 
 def calc_gradient(data):
     d = data[:,:,:,0]
@@ -28,22 +29,22 @@ def shuffle_two_lists(l1, l2):
     r1, r2 = zip(*l)
     return list(r1), list(r2)
 
+# The Data Generator is only used for training and testing on splitted dataset
 class FlashDatasetGenerator(keras.utils.Sequence):
-    def __init__(self, data_dir, batch_size, zero_propagation=True):
-        self.zero_propagation = zero_propagation
-        clean_files = glob.glob(data_dir+"/*/clean/*")
-        # if zero_propagation, create 0-propagation error dataset at runtime
-        if zero_propagation:
+    def __init__(self, clean_data_dir, error_data_dir, batch_size):
+        self.zero_propagation = False
+        if clean_data_dir:
+            clean_files = glob.glob(clean_data_dir+"/*")
+        if error_data_dir:
+            error_files = glob.glob(error_data_dir+"/*")
+        else:   # insert error at runtime
+            self.zero_propagation = True
             error_files = list(clean_files)
-        else:
-            error_files = glob.glob(data_dir+"/*/error/*")
         files = clean_files + error_files
         labels = np.append(np.zeros(len(clean_files)), np.ones(len(error_files)))
         print "clean files:", len(clean_files), ", error files:", len(error_files)
         self.batch_size = batch_size
-
         self.files, self.labels = shuffle_two_lists(files, labels)
-
 
     def __len__(self):
         return np.floor(len(self.files) / float(self.batch_size))
@@ -64,10 +65,6 @@ class FlashDatasetGenerator(keras.utils.Sequence):
             batch_x.append(calc_gradient(img))
         return np.array(batch_x), batch_y
 
-    def get_true_labels(self):
-        truth = [0] * (self.clean_labels/WINDOWS_PER_IMAGE) + [1] * (self.error_labels/WINDOWS_PER_IMAGE)
-        return np.array(truth)
-
 model = Sequential([
     Conv3D(32, (3,3,3), input_shape=(NX, NY, NZ, len(variables))),
     #BatchNormalization(axis=-1),
@@ -84,13 +81,6 @@ model = Sequential([
     Dense(1, activation='sigmoid')
 ])
 
-try:
-    model = multi_gpu_model(model)
-except:
-    pass
-
-#adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-3, amsgrad=False)
-model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
 
 def compute_metrics(pred_labels, true_labels):
     true_labels = np.array(true_labels).reshape(pred_labels.shape)
@@ -109,40 +99,57 @@ def compute_metrics(pred_labels, true_labels):
     print 'TP: %s (%i/%i), FP: %s (%i/%i)' %(recall, tp, error_samples, fpr, fp, total)
     print 'ACC: %s, TN: %i, FN: %i' %(accuracy, tn, fn)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--train_dataset", help="The path to the splitted training set")
-    parser.add_argument("-e", "--test_dataset", help="The path to the splitted test set")
-    parser.add_argument("-n", "--epochs", help="How many epochs for training")
-    parser.add_argument("-m", "--model", help="Specify thet model file")
-    parser.add_argument("-d", "--detect_dataset", help="Run the detector on a unsplitted dataset")
-    args = parser.parse_args()
 
-    model_file = "./models/model_keras.h5"
-    if args.model:
-        model_file = args.model
-    if args.train_dataset:
-        if args.epochs: EPOCHS = int(args.epochs)
-        data_gen = FlashDatasetGenerator(args.train_dataset, 64, zero_propagation=True)
-        #model.load_weights(model_file)
-        model.fit_generator(generator=data_gen, use_multiprocessing=True, workers=8, epochs=EPOCHS, shuffle=True)
+def get_parsed_arguments():
+    parser = argparse.ArgumentParser()
+
+    # Rquired action, need to choose from one of the following.
+    parser_group = parser.add_mutually_exclusive_group(required=True)
+    parser_group.add_argument("--train", help="Train the model with splitted dataset", action="store_true")
+    parser_group.add_argument("--test", help="Test the model with splitted dataset", action="store_true")
+    parser_group.add_argument("--detect", help="Run the detector (trained model) on a unsplitted dataset")
+
+    # optional
+    parser.add_argument("-n", "--epochs", help="How many epochs for training", type=int, default=3)
+    parser.add_argument("-m", "--model", help="Specify thet model file", type=str, default="./models/model_keras.h5")
+    parser.add_argument("--clean", help="Path to splitted clean dataset", default="")
+    parser.add_argument("--error", help="Run the splitted error dataset", default="")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+
+    # Construct the model
+    try: model = multi_gpu_model(model)
+    except: pass
+    #adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-3, amsgrad=False)
+    model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
+
+    args = get_parsed_arguments()
+    model_file = args.model
+
+    if args.train:
+        data_gen = FlashDatasetGenerator(args.clean, args.error, BATCH_SIZE)
+        model.load_weights(model_file)
+        model.fit_generator(generator=data_gen, use_multiprocessing=True, workers=8, epochs=args.epochs, shuffle=True)
         model.save_weights(model_file)
         print model.evaluate_generator(generator=data_gen, use_multiprocessing=True, workers=8, verbose=1)
-    elif args.test_dataset:
-        data_gen = FlashDatasetGenerator(args.test_dataset, 64, zero_propagation=True)
+    elif args.test:
+        data_gen = FlashDatasetGenerator(args.clean, args.error, BATCH_SIZE)
         model.load_weights(model_file)
-        #print model.evaluate_generator(generator=data_gen, use_multiprocessing=True, workers=8, verbose=1)
         scores = model.predict_generator(generator=data_gen, use_multiprocessing=False, workers=1, verbose=1)
+        print scores.shape, len(data_gen.labels)
         for threshold in [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]:
             print "Threshold =", threshold
-            pred = (scores >= threshold)    # shape of (N, WINDOWS_PER_IMAGE)
-            #pred = np.any(pred, axis=1)
+            pred = (scores >= threshold)
             compute_metrics(pred, data_gen.labels)
-    elif args.detect_dataset:
+    elif args.detect:
         model.load_weights(model_file)
         error, nan_count = 0, 0
         # match both for clean checkpoint files and k-delay corrupted files
-        files = glob.glob(args.detect_dataset+"/*chk_*")+glob.glob(args.detect_dataset+"/*error*")
+        files = glob.glob(args.detect+"/*chk_*")+glob.glob(args.detect+"/*error*")
         files.sort()
         for filename in files:
             dens = hdf5_to_numpy(filename)
@@ -152,7 +159,7 @@ if __name__ == "__main__":
             dens_blocks = np.expand_dims(np.squeeze(split_to_blocks(dens)), -1)
             for i in range(dens_blocks.shape[0]):
                 dens_blocks[i] = calc_gradient(dens_blocks[i])
-            pred = model.predict(dens_blocks) > 0.5
+            pred = model.predict(dens_blocks) > 0.65
             error += np.any(pred)
             if np.any(pred) == 0:
                 print filename  # no error detected
