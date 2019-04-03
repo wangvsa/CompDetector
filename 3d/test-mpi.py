89cfe1fb57200dc64791a0937d74371d4d25a8a0
Chen Wang <wangvsa@gmail.com>
2019-04-03 20:14:30 +0000

    model in wrong directory

--- 3d/test-mpi.py
+++ 3d/test-mpi.py
@@ -3,16 +3,17 @@
 
 from mpi4py import MPI
 import numpy as np
-import glob
+import glob, sys
 from keras_detector_3d import detection, model
-from create_dataset import hdf5_to_numpy, split_to_blocks
+from create_dataset import hdf5_to_numpy, split_to_blocks, read_data
 from timeit import default_timer as timer
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
+name = MPI.Get_processor_name()
 
-print rank, size
+print(rank, name, size)
 val = rank
 
 sendbuf = np.zeros(1)
@@ -24,16 +25,18 @@ try:
 except:
     pass
 model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy'])
-model.load_weights("./models/model_keras_sod_15bits.h5")
+model.load_weights(sys.argv[2])
 
 error = 0
-path = "/home/chenw/Flash/Sod_3d/0/10_delay/"
+path = sys.argv[1]
 files = glob.glob(path+"/*chk_*")+glob.glob(path+"/*error*")
 files.sort()
 
+total_time = 0
 start = timer()
 for filename in files:
-    dens = hdf5_to_numpy(filename)
+    dens = read_data(filename)
+    t1 = timer()
     dens_blocks = np.expand_dims(np.squeeze(split_to_blocks(dens)), -1)
 
     #print rank, filename
@@ -45,10 +48,12 @@ for filename in files:
     comm.Reduce(sendbuf, recvbuf, op=MPI.LOR, root=0)
     if rank == 0:
         error += recvbuf[0]
-        print filename, recvbuf
+        #print filename, recvbuf
+    t2 = timer()
+    total_time += (t2-t1)
 
 end = timer()
 
 if rank == 0:
-    print "Running time: %s seconds." %(end-start)
+    print "Running time: %s seconds, training time: %s" %(end-start, total_time)
     print "detected %s error samples, total: %s, recall: %s" %(error, len(files), error*1.0/len(files))
