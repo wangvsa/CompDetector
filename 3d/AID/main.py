0686c0ad3cf83b46c08da218d68c4849992c476e
Chen Wang <wangvsa@gmail.com>
2019-04-07 16:17:33 -0500

    Updatea AID

--- 3d/AID/main.py
+++ 3d/AID/main.py
@@ -1,7 +1,7 @@
 import random, math
 import os, glob, sys
 import numpy as np
-from create_dataset import hdf5_to_numpy, get_flip_error
+from create_dataset import get_flip_error
 from aid import AdaptiveDetector
 
 def read_data(prefix, it):
@@ -15,6 +15,7 @@ We insert an error in each frame and see if AID can detect it
 '''
 def test_0_recall(prefix, total_iterations):
     aid = AdaptiveDetector()
+    aid.fp = 9
 
     d5 = read_data(prefix, 0)
     d4 = read_data(prefix, 1)
@@ -47,32 +48,27 @@ def test_0_recall(prefix, total_iterations):
         d5, d4, d3, d2, d1  = d4, d3, d2, d1, d
         print("it:", it, " recall:", recall, " fp:", aid.fp)
 
-def test_k_recall(restart_iter, delay, clean_prefix, error_prefix):
-    recall = np.zeros(delay)
-
+def evaluate_detection(timestep, clean_prefix, path):
+    recall = 0
     aid = AdaptiveDetector()
-    aid.it = 0
-    aid.fp = 200
+    aid.order = 2
+    aid.it = 5
+    aid.fp = 18
 
     # first read previous clean data
-    d5 = read_data(clean_prefix, restart_iter-5)
-    d4 = read_data(clean_prefix, restart_iter-4)
-    d3 = read_data(clean_prefix, restart_iter-3)
-    d2 = read_data(clean_prefix, restart_iter-2)
-    d1 = read_data(clean_prefix, restart_iter-1)
+    d5 = read_data(clean_prefix, timestep-5)
+    d4 = read_data(clean_prefix, timestep-4)
+    d3 = read_data(clean_prefix, timestep-3)
+    d2 = read_data(clean_prefix, timestep-2)
+    d1 = read_data(clean_prefix, timestep-1)
 
-    for k in range(delay):      # detecting after k iterations
-        d = read_data(error_prefix, k)
-        recall[k] = aid.detect(d, d1, d2, d3, d4, d5)
-        aid.it += 1
+    for f in glob.glob(path+"/error*.npy"):
+        d = read_data(clean_prefix, timestep)
+        hasError = aid.detect(d, d1, d2, d3, d4, d5)
+        print(f, hasError)
+        recall += hasError
 
-        d5 = d4
-        d4 = d3
-        d3 = d2
-        d2 = d1
-        d1 = d
     print recall
-    return recall
 
 
 # Perform the detection on clean data to get false positive rate
@@ -97,12 +93,6 @@ def test_fp(prefix):
         d1 = d
         print("it:", it, " fp:", aid.fp)
 
-#test_0_recall("/home/wangchen/Flash/StirTurb_3d/4/clean/stirturb_3d_hdf5_chk_")
-#test_0_recall("/home/wangchen/Flash/Sedov_3d/0/clean/sedov_hdf5_chk_")
-#test_0_recall("/home/wangchen/Flash/BlastBS_3d/4/clean/blastBS_mhd_hdf5_chk_")
-#test_0_recall("/home/wangchen/Flash/Sod_3d/4/clean/sod_hdf5_chk_")
-#test_fp("/home/wangchen/Flash/BlastBS_3d/4/clean/blastBS_mhd_hdf5_chk_")
-
 if __name__ == "__main__":
     directory = sys.argv[1]
     files = glob.glob(directory+"/*chk*")
@@ -110,25 +100,5 @@ if __name__ == "__main__":
     prefix = files[0].split("chk_")[-2] + "chk_"
     test_0_recall(prefix, total_iterations)
 
-
-
-# test k iterations
-'''
-if __name__ == "__main__":
-    delay = 11
-    recall = np.zeros(delay)
-    total = 0
-    # First find all restarting points
-    directory = "/home/wangchen/Flash/OrszagTang/"
-    for filename in glob.iglob(directory+"*plt_cnt_0000"):
-        last_one = filename[:-4] + "0011"
-        if os.path.isfile(last_one):
-            restart_iter = int(filename.split("_")[1])
-            error_prefix = filename[0:-4]
-            clean_prefix = directory + "clean/orszag_mhd_2d_hdf5_plt_cnt_"
-            print(restart_iter, error_prefix)
-            recall += test_k_recall(restart_iter, delay, clean_prefix, error_prefix)
-            total += 1.0
-            print(recall)
-            print(recall/total)
-'''
+    #evaluate_detection(100, "/home/wangchen/Flash/SC19/Sod_3d/clean/sod_hdf5_chk_", sys.argv[1])
+    #evaluate_detection(100, "/home/wangchen/Flash/BlastBS_3d/4/clean/blastBS_mhd_hdf5_chk_", sys.argv[1])
